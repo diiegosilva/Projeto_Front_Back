cbcb6725917e6444a425c346536d71cc9b9cc82a
diiegosilva <diegosilva157890@gmail.com>
2026-04-03 09:53:17 -0300

    feat: inicio do projeto

---
+++ cadastro-usuario/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="hero">
+        <div className="hero-content">
+          <h1>Bem-vindo ao Cadastro de Usuário</h1>
+          <p>Crie sua conta e faça parte da nossa comunidade!</p>
+
+
+          <a href="#form" className="btn">Cadastrar-se</a>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
